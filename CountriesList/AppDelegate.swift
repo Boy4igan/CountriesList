c451291538c4ad711f657b495148d16fd9c20f9b
Max Boyko <40644710+Boy4igan@users.noreply.github.com>
2018-07-30 21:26:44 +0300

    Remove unnecessary lines from AppDelegate

--- CountriesList/AppDelegate.swift
+++ CountriesList/AppDelegate.swift
@@ -5,14 +5,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    //MARK: ME очевидно есть пара лишних строк
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-        
-        let mainController          = CLMainTableViewController()
-        let navigationController    = UINavigationController(rootViewController: mainController)
-        
         window                      = UIWindow(frame: UIScreen.main.bounds)
-        window!.rootViewController  = navigationController
+        window!.rootViewController  = UINavigationController(rootViewController: CLMainTableViewController())
         window!.makeKeyAndVisible()
         
         return true
