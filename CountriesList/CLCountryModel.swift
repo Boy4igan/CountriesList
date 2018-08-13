3d72fa6e96f3c492d782eebf1d23a23953ea4369
Max Boyko <40644710+Boy4igan@users.noreply.github.com>
2018-08-13 17:01:16 +0300

    Add constructor with country title and country attributes to CountryModel.swift

--- CountriesList/CLCountryModel.swift
+++ CountriesList/CLCountryModel.swift
@@ -7,4 +7,14 @@ struct CLCountryModel {
     let population: UInt
     let countryDescription: String
     let flag: UIImage
+    
+    init(countryTitle: String, countryAttributes: [String : AnyObject]) {
+        self.title              = countryTitle
+        self.capital            = countryAttributes["Capital"]      as! String
+        self.area               = countryAttributes["Total area"]   as! UInt
+        self.population         = countryAttributes["Population"]   as! UInt
+        self.countryDescription = countryAttributes["Description"]  as! String
+        self.flag               = UIImage(named: title)!
+    }
+    
 }
